b5dac7e372a62053cb8b66bb7a825d3e381d7035
se3rik <se3rik@mail.ru>
2024-11-02 23:39:29 +0300

    feat: slice for posts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import CssBaseline from "@mui/material/CssBaseline";
 
-import "./index.scss";
+import App from "./App";
+import store from "./redux/store";
+
+import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -15,7 +18,9 @@ root.render(
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
